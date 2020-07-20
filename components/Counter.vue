f7105c78d9acb7ed2cae725a1a4551b71250d52d
AdamBD <adam.bendavid@gmail.com>
2020-07-20 19:12:08 +0300

    updated stats counter

--- components/Counter.vue
+++ components/Counter.vue
@@ -6,7 +6,7 @@
                 <!-- single counetr -->
                 <div class="single-counter text-center">
                     <span class="counter">
-                      <countTo :startVal='0' :endVal='startCounter ? 56 : 0' :duration='3000'></countTo>
+                      <countTo :startVal='0' :endVal='startCounter ? 102 : 0' :duration='3000'></countTo>
                     </span>
                     <p>Attendees and  <br/> counting</p>
                 </div><!-- single counetr -->
