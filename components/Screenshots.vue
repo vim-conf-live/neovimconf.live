8676418bc6d7e5ee3ce8ec7d8bc89a6a7ffa47be
AdamBD <adam.bendavid@gmail.com>
2020-07-25 11:13:34 +0300

    added speaker-jeremy

--- components/Screenshots.vue
+++ components/Screenshots.vue
@@ -65,6 +65,13 @@
                         </div>
                         <!-- slingle app image -->
                     </swiper-slide>
+                    <swiper-slide>
+                        <!-- slingle app image -->
+                        <div class="item">
+                            <img src="/assets/img/speaker-jeremy.png" alt="" />
+                        </div>
+                        <!-- slingle app image -->
+                    </swiper-slide>
                 </swiper>
                 <div class="swiper-pagination owl-dots" slot="pagination"></div>
             </div>
