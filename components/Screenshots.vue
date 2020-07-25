0635eb1aeec289fa58b2846bc91b6ece16a51839
AdamBD <adam.bendavid@gmail.com>
2020-07-25 21:40:02 +0300

    added speaker-oliver & updated speaker-jeremy

--- components/Screenshots.vue
+++ components/Screenshots.vue
@@ -72,6 +72,13 @@
                         </div>
                         <!-- slingle app image -->
                     </swiper-slide>
+                    <swiper-slide>
+                        <!-- slingle app image -->
+                        <div class="item">
+                            <img src="/assets/img/speaker-oliver.png" alt="" />
+                        </div>
+                        <!-- slingle app image -->
+                    </swiper-slide>
                 </swiper>
                 <div class="swiper-pagination owl-dots" slot="pagination"></div>
             </div>
