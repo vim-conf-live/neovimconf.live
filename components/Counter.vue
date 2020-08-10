c1f2b0a7dac4b0732a220078192de860f98f6d18
AdamBD <adam.bendavid@gmail.com>
2020-08-10 09:39:44 +0300

    added agenda & updated counters

--- components/Counter.vue
+++ components/Counter.vue
@@ -6,7 +6,7 @@
                 <!-- single counetr -->
                 <div class="single-counter text-center">
                     <span class="counter">
-                      <countTo :startVal='0' :endVal='startCounter ? 809 : 0' :duration='3000'></countTo>
+                      <countTo :startVal='0' :endVal='startCounter ? 955 : 0' :duration='3000'></countTo>
                     </span>
                     <p>Attendees and  <br/> counting</p>
                 </div><!-- single counetr -->
@@ -15,7 +15,7 @@
                 <!-- single counetr -->
                 <div class="single-counter text-center">
                     <span class="counter">
-                      <countTo :startVal='0' :endVal='startCounter ? 13 : 0' :duration='3000'></countTo>
+                      <countTo :startVal='0' :endVal='startCounter ? 14 : 0' :duration='3000'></countTo>
                     </span>
                     <p>Speakers</p>
                 </div><!-- single counetr -->
--- components/ServiceTwo.vue
+++ components/ServiceTwo.vue
@@ -1,40 +1,304 @@
 <template>
-  <section id="agenda" class="pt-120 pb-120">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-5 col-sm-5">
-                <!-- user ineract text -->
-                <div class="user-interact-inner">
-                    <div class="interact-icon">
-                        <img src="/assets/img/icons/solution1.svg" class="svg" alt="">
+    <section id="agenda" class="pt-120 pb-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-sm-4">
+                    <!-- user ineract text -->
+                    <div class="user-interact-inner">
+                        <div class="interact-icon">
+                            <img src="/assets/img/icons/solution1.svg" class="svg" alt="" />
+                        </div>
+                        <h2>Agenda</h2>
                     </div>
-                    <h2>Agenda</h2>
-                    <p>
-                        We will be listing out the entire conference schedule and
-                        agenda. You can check back here, but we will also mail you directly once you
-                        sign up!
-                    </p>
+                    <!--End of user ineract text -->
                 </div>
-                <!--End of user ineract text -->
-            </div>
-            <div class="col-lg-7 col-sm-7">
-                <!-- user interact image -->
-                <div class="user-interact-image type2">
-                    <img src="/assets/img/agenda-teaser.png"  alt="">
+                <div class="col-lg-7 col-sm-7">
+                    <!-- user interact image -->
+                    <div class="user-interact-image type2">
+                        <!--<img src="/assets/img/agenda-teaser.png"  alt="">-->
+                        <div id="agenda-inner">
+                            <div
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>ThePrimeagen</button>
+                                <h4 style="margin-left: 20px; margin-top: 6px;">Vim Speed 🏃</h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                A classic spirited talk on speed and mastery of Vim movements. Also
+                                some data on key strokes and speed. Is the shortest amount of
+                                keystrokes always the fastest way to interact with vim? What does
+                                (limited) data suggest about speed of keystrokes?
+                            </p>
+
+                            <div
+                                class="agenda-header"
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>
+                                    <a
+                                        href="https://github.com/vigoux"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        >Thomas Vigouroux</a
+                                    >
+                                </button>
+                                <h4 style="margin-left: 20px; margin-top: 6px;">
+                                    From user to contributor 🆒
+                                </h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                Let me bring you through the journey from user to contributor,
+                                through my example with treesitter, and let me convince you that you
+                                can do it too.
+                            </p>
+                            <div
+                                class="agenda-header"
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>TJ Devries</button>
+                                <h4 style="margin-left: 20px; margin-top: 6px;">
+                                    Why Lua? 🤔
+                                </h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                Why does Neovim use Lua and how can you start writing plugins and
+                                config in Lua
+                            </p>
+                            <div
+                                class="agenda-header"
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>TJ Devries</button>
+                                <h4 style="margin-left: 20px; margin-top: 6px;">
+                                    Builtin LSP 🎙️
+                                </h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                What is LSP and why does Neovim ship with it now. More general info.
+                            </p>
+                            <div
+                                class="agenda-header"
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>Bryan Phelps</button>
+                                <h4 style="margin-left: 20px; margin-top: 6px;">
+                                    Onivim: Modal Editing from the Future 👾
+                                </h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                I'll talk about Onivim - how it started, has evolved, and show demos
+                                of the current state. I'll showcase the user experience as well as
+                                talk about the underlying technology (libvim/Revery/etc)
+                            </p>
+                            <div
+                                class="agenda-header"
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>Leeren chang</button>
+                                <h4 style="margin-left: 20px; margin-top: 6px;">
+                                    IDE-like navigation & tooling for any language, plugin-free! 🧰
+                                </h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                In this talk we’ll focus on how Vim can be quickly tailored for any
+                                language to support efficient navigation with the use of tags,
+                                path-based operations, include file search, and project-centric
+                                directory management. In addition, we’ll look into how we can
+                                exploit Vim’s native features to setup a more integrated experience
+                                for code compilation and formatting. We’ll be using Golang as an
+                                example language, walking through how to customize a fresh
+                                installation of Vim to accommodate smooth navigation and development
+                                of Go source code within a matter of minutes, with no external
+                                plugins involved.
+                            </p>
+                            <div
+                                class="agenda-header"
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>David Begin</button>
+                                <h4 style="margin-left: 20px; margin-top: 6px;">
+                                    Vim isn't an Editor it's an Instrument 🎛️
+                                </h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                Applying Techniques from playing improvised music to Vim, as well as
+                                making music with Vim!
+                            </p>
+                            <div
+                                class="agenda-header"
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>Shreyansh Chouhan</button>
+                                <h4 style="margin-left: 20px; margin-top: 6px;">
+                                    The Open-source summer experience ⛺
+                                </h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                What is GSoC? What have I been working on this summer? How did I get
+                                involved with neovim? What did I work on? How can you do what I did?
+                            </p>
+                            <div
+                                class="agenda-header"
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>
+                                    <a
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        href="https://mobile.twitter.com/jeremyckahn"
+                                        >Jeremy Kahn</a
+                                    >
+                                </button>
+                                <h4 style="margin-left: 20px; margin-top: 6px;">
+                                    Giving Vim Superpowers 🦸
+                                </h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                Workshop: A guided session on going from a bare Vim config to a
+                                tricked out productivity powerhouse. We'll cover how to set up a
+                                plugin manager, install and interface with external command line
+                                tools, and seize control of even the gnarliest of codebases!
+                            </p>
+                            <div
+                                class="agenda-header"
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>
+                                    <a
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        href="http://pragmaticpineapple.com/"
+                                        >Nikola Đuza</a
+                                    >
+                                </button>
+                                <h4 style="margin-left: 20px; margin-top: 6px;">
+                                    How Did Vim Become So Popular 🏺
+                                </h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                A walkthrough on how Vim became what it is today, a powerful text
+                                editor that is used by many.
+                            </p>
+                            <div
+                                class="agenda-header"
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>
+                                    <a
+                                        href="https://twitter.com/OliverCaldwell"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        >Oliver Caldwell</a
+                                    >
+                                </button>
+                                <h4 style="margin-left: 20px; margin-top: 6px;">
+                                    Conversational Software Development: What, Why and How 🗣️
+                                </h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                A journey into interactive Lisp evaluation for Neovim dotfiles and
+                                plugins as well as Clojure and Janet projects. Finish up with an
+                                interactive Conjure tutorial (requires curl and an up to date nvim)
+                                that gets you evaluating just like me within seconds!
+                            </p>
+                            <div
+                                class="agenda-header"
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>
+                                    <a
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        href="https://www.youtube.com/c/GregHurrell"
+                                        >Greg Hurrell</a
+                                    >
+                                </button>
+
+                                <h4 style="margin-left: 20px; margin-top: 6px;">
+                                    Sharpening the axe 🪓
+                                </h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                Insights learned from 10 years of dotfile tweakingbu
+                            </p>
+                            <div
+                                class="agenda-header"
+                                style="display: flex;
+  flex-direction: row;  text-align: middle;align-items: center;
+
+ "
+                            >
+                                <button>Matt Boehm</button>
+                                <h4 style="margin-left: 20px; margin-top: 6px;">
+                                    Learning Vim: the path to mastery 🧘
+                                </h4>
+                            </div>
+                            <p style="margin-top: 20px;">
+                                Tips on useful resources and building muscle memory, with some demos
+                                of useful plugins and tricks along the way. (I can work with
+                                Adam/Greg to make sure content doesn't overlap too much)
+                            </p>
+                        </div>
+                    </div>
+                    <!-- End of user interact image -->
                 </div>
-                <!-- End of user interact image -->
             </div>
         </div>
-    </div>
-</section>
+    </section>
 </template>
 
 <script>
-    export default {
-        name: "ServiceTwo"
+export default {
+    name: "ServiceTwo",
+    data() {
+        return {};
     }
+};
 </script>
 
 <style scoped>
-
+#agenda-inner {
+    height: 500px;
+    width: 750px;
+    overflow: auto;
+}
+.agenda-header {
+    padding-top: 30px;
+}
 </style>
