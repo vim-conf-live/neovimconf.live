8a45166f0f567c6b9b483b63db741c356eb0205a
Nathan Henniges <demonwolf@demonwolfdev.com>
2021-08-10 11:41:24 -0500

    ITS WORKING.

---
+++ components/Landing/Sponsors.vue
@@ -0,0 +1,50 @@
+<template>
+  <client-only>
+    <swiper class="swiper" :options="swiperOption">
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide>
+      <swiper-slide>Slide 5</swiper-slide>
+      <swiper-slide>Slide 6</swiper-slide>
+      <swiper-slide>Slide 7</swiper-slide>
+      <swiper-slide>Slide 8</swiper-slide>
+      <swiper-slide>Slide 9</swiper-slide>
+      <swiper-slide>Slide 10</swiper-slide>
+      <div slot="pagination" class="swiper-pagination"></div>
+    </swiper>
+  </client-only>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      swiperOption: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      },
+    }
+  },
+}
+</script>
+
+<style scoped>
+.swiper {
+  height: 300px;
+  width: 100%;
+}
+.swiper .swiper-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-weight: bold;
+  font-size: 16px;
+  background-color: #fff;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,6 @@
   <div id="content">
     <LandingHero />
     <LandingWatch />
+    <LandingSponsors />
   </div>
 </template>
