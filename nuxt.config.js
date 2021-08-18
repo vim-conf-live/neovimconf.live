a1e4b67c0702f143050fd3e03cf460a26275e0f0
mvargasmoran <mvargasmoran@gmail.com>
2021-08-18 17:34:05 -0400

    Set twitter card type to summary large image
    Also add twitter creator prop, og sizes, absolute urls
    And unify the titles and descriptions

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,86 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: 'Vimconf.Live - The virtual vim conference',
+        content: 'October 29/30th',
+      },
+
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: "Vim Virtual's Conf",
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website',
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://www.vimconf.live/',
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: "Join the world's first virtual Vim conference Oct 29/30th",
+      },
+      {
+        hid: 'theme-color',
+        property: 'theme-color',
+        content: '#111827',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'Connecting the vim community to share their love and passion for all things vim!',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://www.vimconf.live/vimconf.jpg',
+
+      },
+      {
+        property: 'og:image:width',
+        content: '1200',
+      },
+      {
+        property: 'og:image:height',
+        content: '630',
+      },
+
+      {
+        name: 'twitter:site',
+        content: '@VimconfLive',
+      },
+      {
+        name: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      {
+        hid: 'twitter:url',
+        name: 'twitter:url',
+        content: 'https://www.vimconf.live',
+      },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: "Join the world's first virtual Vim conference Oct 29/30th",
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content: 'Connecting the vim community to share their love and passion for all things vim!',
+      },
+      {
+        hid: 'twitter:creator',
+        name: 'twitter:creator',
+        content: '@VimconfLive',
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: 'https://www.vimconf.live/vimconf.jpg',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
