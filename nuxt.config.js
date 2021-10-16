deec62af6c2dccd82fdb38feea6c415bbe323fe4
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-10-15 22:29:01 -0300

    Add smoth scrolling
    As indicated here: <https://stackoverflow.com/a/63599869/11917764>

--- nuxt.config.js
+++ nuxt.config.js
@@ -105,6 +105,9 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    '~/assets/global.css'
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
