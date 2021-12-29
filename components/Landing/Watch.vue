a3d0feb43f6244d757db2e87cfbdc0762fb45501
mvargasmoran <mvargasmoran@gmail.com>
2021-12-29 15:54:27 -0300

    Add highlights video to landing page
    That video is kinda bloopers, but no...
    and also has the bridge to TeeJ's talk.

--- components/Landing/Watch.vue
+++ components/Landing/Watch.vue
@@ -31,7 +31,29 @@
       >
         Start Watching
       </a>
+      <!-- width="560" height="315" -->
+      <div class="youtube">
+        <div>
+          <iframe style="width: 100%; height: 100%;" src="https://www.youtube.com/embed/Muk6lXGQTwM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
+<style lang="scss" scoped>
+.youtube {
+  margin-top: 20px;
+  width: 100%;
+  height: 400px;
+  div {
+    width: 100%;
+    height: 100%;
+    display: block;
+    > iframe {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
