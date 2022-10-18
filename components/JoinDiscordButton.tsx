25e69227d0211f6f08f76422382a5f8f6c12353e
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-10-18 19:41:31 -0300

    Name component

--- components/JoinDiscordButton.tsx
+++ components/JoinDiscordButton.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
 
-export default () => (
+const JoinDiscordButton = () => (
   <Link href="https://discord.gg/Cz9J4ZD">
     <a style={{ background: "rgba(59, 130, 246, 1)"}} className="text-white rounded-lg p-2" target="_blank">
       Join Discord
     </a>
   </Link>
 );
+
+export default JoinDiscordButton;
