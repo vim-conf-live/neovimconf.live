f22166796146f76e117659be5624648bea168f42
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-10-18 19:31:09 -0300

    Create discord button component

---
+++ components/JoinDiscordButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default () => (
+  <Link href="https://discord.gg/Cz9J4ZD">
+    <a style={{ background: "rgba(59, 130, 246, 1)"}} className="text-white rounded-lg p-2" target="_blank">
+      Join Discord
+    </a>
+  </Link>
+);
