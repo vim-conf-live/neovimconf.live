de778a5452ab10d81f93eed3db674920999cf018
Carles Andres <carles.andres@boardintelligence.com>
2022-10-18 20:25:24 +0100

    Set up userbase in registration page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import 'styles/main.css';
 
 function MyApp({ Component, pageProps }) {
-  const getLayout = Component.getLayout || ((page:any) => page);
+  const getLayout = Component.getLayout || ((page: any) => page);
   const renderedComp = getLayout(<Component {...pageProps} />);
 
   // <link rel="canonical" href={canonicalURL} />
@@ -15,10 +15,7 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="initial-scale=1.0, width=device-width, maximum-scale=1"
         />
-        <meta
-          name="description"
-          content="Neovim Conf 2022"
-        />
+        <meta name="description" content="Neovim Conf 2022" />
       </Head>
       {renderedComp}
     </>
--- pages/registration.tsx
+++ pages/registration.tsx
@@ -1,42 +1,53 @@
+import { ReactNode, MouseEvent } from 'react';
+import NavbarLayout from 'components/NavbarLayout';
 import { useState, useEffect, useCallback, ChangeEvent } from 'react';
 import userbase from 'userbase-js';
 
-const RegistrationPage = () => {
+const USERBASE_APP_ID = process.env.NEXT_PUBLIC_USERBASE_APP_ID;
+
+function RegistrationPage() {
   const [email, setEmail] = useState('');
 
   useEffect(() => {
-    console.log('userbase', userbase);
+    if (USERBASE_APP_ID) {
+      userbase.init({ appId: USERBASE_APP_ID });
+    }
   }, []);
 
   const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) =>
     setEmail(event.target.value);
 
   const handleSubmitRegistration = useCallback(
-    (event) => {
+    (event: MouseEvent<HTMLButtonElement>) => {
       event.preventDefault();
-      console.log('email', email);
+      userbase.signUp({
+        email,
+        username: email,
+        password: 'vimconf2',
+        rememberMe: 'none',
+      });
     },
-    [email]
+    [email, userbase]
   );
 
   return (
     <div
       className={`container m-4 mx-auto flex min-h-screen
-items-center justify-center 
+      items-center justify-center 
     `}
     >
       <form className="flex w-96 flex-col gap-4">
         <label className="text-sm text-gray-300">Register to speak</label>
         <input
-          className="p-2"
+          className="p-2 text-gray-800 rounded-sm"
           type="text"
           value={email}
           onChange={handleEmailChange}
         />
         <button
           type="submit"
-          className={`rounded bg-gray-200 px-4 py-2 shadow transition 
-            hover:bg-gray-300`}
+          className={`rounded bg-green-500 px-4 py-2 text-blue-900 shadow transition 
+            hover:bg-green-600`}
           onClick={handleSubmitRegistration}
         >
           Register
@@ -44,6 +55,10 @@ items-center justify-center
       </form>
     </div>
   );
+}
+
+RegistrationPage.getLayout = (page: ReactNode) => {
+  return <NavbarLayout>{page}</NavbarLayout>;
 };
 
 export default RegistrationPage;
