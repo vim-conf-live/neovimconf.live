36e7e268e79e37cae2e5a3925666d9cedf9b55a1
Carles Andres <carles.andres@boardintelligence.com>
2022-10-22 12:33:00 +0100

    Some style changes to buttons

--- components/JoinDiscordButton.tsx
+++ components/JoinDiscordButton.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
+import { FaDiscord } from 'react-icons/fa';
 
 const JoinDiscordButton = () => (
   <Link href="https://discord.gg/Cz9J4ZD">
     <a
       className={`rounded-lg bg-brand-discord px-4 py-2 text-white 
-        text-sm transition duration-300 hover:opacity-70` }
+        text-sm transition duration-300 hover:opacity-70 flex items-center
+        gap-2` }
       target="_blank"
     >
-      Join Discord
+      <FaDiscord className="text-lg"/>
+      <span>Join Discord</span>
     </a>
   </Link>
 );
