462c7fa2e7821a1569a2d0ee6d97d8f5e15da2d8
mvargasmoran <mvargasmoran@gmail.com>
2022-10-21 22:02:12 -0300

    Fix allow allowFullScreen for youtube embed

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,7 @@ function Home() {
             src="https://www.youtube.com/embed/Muk6lXGQTwM"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
           <h2 className="mt-20 mb-8">
             Binge Watch the video playlist for the 2021 talks
@@ -69,7 +69,7 @@ function Home() {
             src="https://www.youtube.com/embed/videoseries?list=PLcTu2VkAIIWyuX-yNe0KNSPR7wjS2lNKz"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </div>
       </main>
