eabffa86e6f6cfd96a9d2fc63952cf5605b56e07
isaif <saif935775@gmail.com>
2022-10-26 00:14:22 +0530

    Add speaker section to home page

--- components/SpeakersSection.tsx
+++ components/SpeakersSection.tsx
@@ -2,6 +2,7 @@ const SpeakersSection = () => {
   return (
     <section className="py-16">
       <h2 className="">Speakers</h2>
+      <p className="my-4">Coming soon</p>
     </section>
   );
 };
