e88a7a391cb4c3789f91bba77b317a12d4ef4d2f
isaif <saif935775@gmail.com>
2022-10-25 23:06:03 +0530

    Move rebranding rationale to navbar

--- components/JoinDiscordButton.tsx
+++ components/JoinDiscordButton.tsx
@@ -5,7 +5,7 @@ import { FaDiscord } from 'react-icons/fa';
 const JoinDiscordButton = () => (
   <Link href="https://discord.gg/Cz9J4ZD">
     <a
-      className={`flex items-center gap-2 rounded-lg bg-brand-discord 
+      className={`ml-auto flex items-center gap-2 rounded-lg bg-brand-discord 
         px-4 py-2 text-sm text-white transition duration-300
         hover:opacity-70`}
       target="_blank"
