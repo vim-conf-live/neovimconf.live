5a86ab6bf9b6d4f7c8bbfd14a00cd677519b82dc
isaif <saif935775@gmail.com>
2022-10-26 01:37:42 +0530

    Add cards for speaker section

--- components/SpeakersSection.tsx
+++ components/SpeakersSection.tsx
@@ -1,8 +1,11 @@
+import Card from './Card';
+
 const SpeakersSection = () => {
   return (
     <section className="py-16">
       <h2 className="">Speakers</h2>
       <p className="my-4">Coming soon</p>
+      <Card />
     </section>
   );
 };
