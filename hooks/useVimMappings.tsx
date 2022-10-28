79bf1ffe200cdebc1435d2d7f82d6092bfa49af8
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-10-28 20:22:43 -0300

    Scroll up and down

--- hooks/useVimMappings.tsx
+++ hooks/useVimMappings.tsx
@@ -3,6 +3,12 @@ import { useCallback, useEffect } from 'react';
 export default function useVimMappings() {
   const onKeyPress = useCallback((event: KeyboardEvent) => {
     switch (event.key) {
+      case 'j':
+        document.documentElement.scrollBy({ top: 300, behavior: 'smooth' });
+        break;
+      case 'k':
+        document.documentElement.scrollBy({ top: -300, behavior: 'smooth' });
+        break;
       case 'g':
         document.documentElement.scrollTo({ top: 0, behavior: 'smooth' });
         break;
