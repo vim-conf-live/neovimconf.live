47a4ea08bfcd57b0b92b99c82ef4af4971c38200
mvargasmoran <mvargasmoran@gmail.com>
2022-11-11 12:26:35 -0300

    Revert SponsorsSection

--- components/SponsorsSection.tsx
+++ components/SponsorsSection.tsx
@@ -2,6 +2,7 @@ const SponsorsSection = () => {
   return (
     <section className="">
       <h2 className="text-[#0f74d0]">Sponsors</h2>
+      <p className="my-4">Coming soon</p>
     </section>
   );
 };
