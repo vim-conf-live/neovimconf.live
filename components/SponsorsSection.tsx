490b1e681fe48e68efe70e5582a060d5daec353e
mvargasmoran <mvargasmoran@gmail.com>
2022-11-11 12:09:02 -0300

    Remove Coming soon from sponsors

--- components/SponsorsSection.tsx
+++ components/SponsorsSection.tsx
@@ -2,7 +2,6 @@ const SponsorsSection = () => {
   return (
     <section className="">
       <h2 className="text-[#0f74d0]">Sponsors</h2>
-      <p className="my-4">Coming soon</p>
     </section>
   );
 };
