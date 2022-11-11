5b937bb04b671fa220e8b3cceb819aa686244933
mvargasmoran <mvargasmoran@gmail.com>
2022-11-11 17:45:56 -0300

    Add Theprimeagen and TeejDeVries as speakers

--- components/MCSection.tsx
+++ components/MCSection.tsx
@@ -3,7 +3,12 @@ const MCSection = () => {
     <section>
       <h2 className="my-4 sm:my-8 text-[#0f74d0]">Masters of Ceremonies</h2>
       <div className="grid gap-6 md:mx-5 md:grid-cols-2">
-        
+        <div className="mc-card">
+          <h1>Theprimeagen</h1>
+        </div>
+        <div className="mc-card">
+          <h1>TeeJ DeVries</h1>
+        </div>
       </div>
     </section>
   );
