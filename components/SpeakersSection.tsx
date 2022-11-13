a8c2428411be17d18811b49e91cf2b6fab8430e0
lahnie <bvargasmoran@gmail.com>
2022-11-12 23:27:24 -0300

    Fixed Linter error
    added key property to speaker card in iterator

--- components/SpeakersSection.tsx
+++ components/SpeakersSection.tsx
@@ -1,5 +1,5 @@
 import Card from './Card';
-import speakers from '/data/Speakers.js';
+import speakers from '../data/Speakers.js';
 
 const SpeakersSection = () => {
   console.log(speakers);
@@ -7,9 +7,10 @@ const SpeakersSection = () => {
     <section className="h-fit">
       <h2 className="my-4 text-[#21ee4b] sm:my-8">Speakers</h2>
       <div className="grid gap-6 md:mx-5 md:grid-cols-2">
-        {speakers.map((speaker) => {
+        {speakers.map((speaker, index) => {
           return (
             <Card
+              key={index}
               name={speaker.name}
               src={speaker.imagesrc}
               about={speaker.about}
