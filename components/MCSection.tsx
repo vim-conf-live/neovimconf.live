283551cc4d4c40149eab2ee07f52e635d2f98ff8
mvargasmoran <mvargasmoran@gmail.com>
2022-11-13 22:22:25 -0300

    Add MC images

--- components/MCSection.tsx
+++ components/MCSection.tsx
@@ -1,12 +1,18 @@
 const MCSection = () => {
   return (
-    <section>
-      <h2 className="my-4 sm:my-8 text-[#0f74d0]">Masters of Ceremonies</h2>
-      <div className="grid gap-6 md:mx-5 md:grid-cols-2">
-        <div className="mc-card">
+    <section className="h-auto">
+      <h2 className="my-4 text-[#0f74d0] sm:my-8">Masters of Ceremonies</h2>
+      <div className="grid content-center justify-items-center md:mx-5 md:grid-cols-2">
+        <div 
+            className="mc-card grid justify-items-center justify-content-center"
+            style={{backgroundImage: `url(/speaker-images/mescience.jpg)`}}
+          >
           <h1>Theprimeagen</h1>
         </div>
-        <div className="mc-card">
+        <div 
+           className="mc-card grid justify-items-center justify-content-center"
+            style={{backgroundImage: `url(/speaker-images/teej.jpg)`}}
+        >
           <h1>TeeJ DeVries</h1>
         </div>
       </div>
