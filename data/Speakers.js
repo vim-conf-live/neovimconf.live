9f962a3d3485f2358a9de03a31de25bd28ede06a
Adam Ben-David <adam.bendavid@gmail.com>
2022-11-18 09:18:08 +0100

    Update Speakers.js

--- data/Speakers.js
+++ data/Speakers.js
@@ -77,7 +77,7 @@ const speakers = [
     name: 'Benny Powers',
     imagesrc: '/speaker-images/bennyspeaker.jpg',
     about:
-      'Principal Web Developer @Red Hat, working on design systems and web components. Atom refugee pining for the good old days of vim-mode-plus and neovim plugin author.',
+      'Principal UX engineer @Red Hat, working on design systems and web components. Atom refugee pining for the good old days of vim-mode-plus and neovim plugin author.',
       github:'https://github.com/bennypowers',
       twitter:'https://social.bennypowers.dev/@i',
       website:'https://bennypowers.dev/'
