9cd83e40f1dea7edb9e38881cd0a745b75ede280
Carles Andres <carles.andres@boardintelligence.com>
2022-11-29 21:26:08 +0000

    Improved Schedule section

--- components/AgendaSection.tsx
+++ components/AgendaSection.tsx
@@ -10,13 +10,13 @@ const AgendaSection = () => {
 
   return (
     <section className="w-full">
-      <h2 className="sm:mb-4">Agenda</h2>
+      <h2 className="sm:mb-4">Schedule</h2>
       <Link
         href="https://calendar.google.com/calendar/ical/c_pmamnjmfa6b2ii3v1ei1te5mm4%40group.calendar.google.com/public/basic.ics"
         target="_blank"
       >
         <a
-          className={`inline-flex content-start my-12 mx-auto items-center gap-2 rounded-lg 
+          className={`inline-flex content-start mt-12 mx-auto items-center gap-2 rounded-lg 
                     bg-[#0f74d0] hover:bg-[#0447BF] duration-300 py-3 px-4
                     text-sm transition md:text-base`}
         >
@@ -24,6 +24,17 @@ const AgendaSection = () => {
           <span>Add to your Calendar</span>
         </a>
       </Link>
+        <div className="mt-8">
+          Or check the{` `} 
+      <Link href="/schedule" >
+        <a
+          className={`inline-flex content-start mb-16 mx-auto items-center 
+                    text-[#0f74d0] hover:text-[#0447BF] duration-300 
+                    text-sm transition md:text-base`}
+        >schedule details
+        </a>
+      </Link>
+      </div>
     </section>
   );
 };
