e05651b5533bad4150f5a51a5976472390340139
Carles Andres <carlos.andres.c@gmail.com>
2022-11-29 20:07:32 +0000

    urls to access calendar

--- components/AgendaSection.tsx
+++ components/AgendaSection.tsx
@@ -1,6 +1,11 @@
 import agenda2022 from 'utils/agenda-2022.json';
 import DayTable from 'components/DayTable';
 
+// https://calendar.google.com/calendar/ical/c_pmamnjmfa6b2ii3v1ei1te5mm4%40group.calendar.google.com/public/basic.ics
+// <iframe src="https://calendar.google.com/calendar/embed?src=c_pmamnjmfa6b2ii3v1ei1te5mm4%40group.calendar.google.com&ctz=Europe%2FLondon" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+//
+//https://calendar.google.com/calendar/embed?src=c_pmamnjmfa6b2ii3v1ei1te5mm4%40group.calendar.google.com&ctz=Europe%2FLondon
+
 const AgendaSection = () => {
   const dayTables = agenda2022.map((day) => (
     <DayTable key={day.date} {...day} />
