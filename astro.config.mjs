eb7ff84547281e3e5fe8f49650d1ec866ed8965f
Nils Riedemann <nils@bleepbloop.studio>
2023-10-30 16:00:01 +0100

    feat: add redirect for /talks => /talks/day-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: { "/talks": "/talks/day-1" },
   integrations: [tailwind({
     applyBaseStyles: false,
   }), react()]
