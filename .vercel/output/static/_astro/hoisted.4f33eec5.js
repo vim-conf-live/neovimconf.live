36c520c0de476a788e6b4e2b7dcb3bec56f463e0
Nils Riedemann <nils@bleepbloop.studio>
2023-11-02 17:08:37 +0100

    fix: remove build errors

---
+++ .vercel/output/static/_astro/hoisted.4f33eec5.js
@@ -0,0 +1,1 @@
+import{i as t}from"./hoisted.7dd3839e.js";for(const o of document.querySelectorAll("[data-hotkey]"))t(o);
---
+++ .vercel/output/static/_astro/hoisted.d323039d.js
@@ -0,0 +1,1 @@
+import{i as t}from"./hoisted.e01d9512.js";for(const o of document.querySelectorAll("[data-hotkey]"))t(o);
