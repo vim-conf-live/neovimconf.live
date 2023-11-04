3b4713c4a367aacd808c1dbd6cdf024ca19d60a7
Nils Riedemann <nils@bleepbloop.studio>
2023-11-04 17:30:05 +0100

    feat: finalize ticket design

--- src/pages/ticket/[id].png.ts
+++ src/pages/ticket/[id].png.ts
@@ -8,9 +8,9 @@ import { createSBSSR } from "@/lib/server/supabase";
 
 export const prerender = false;
 
-const e = (tag: string, tw: string, ...children: any[]) => ({
-  tag,
-  props: { children, tw },
+const e = (tag: string, tw: string, children: any[], props?: any) => ({
+  type: tag,
+  props: { children, tw, ...props },
 });
 
 export const GET: APIRoute = async ({ request, params, cookies }) => {
@@ -19,7 +19,7 @@ export const GET: APIRoute = async ({ request, params, cookies }) => {
 
   const { error, data: user } = await supabase
     .from("profiles")
-    .select("username, full_name, job_description")
+    .select("username, full_name, job_description,id")
     .eq("ticket", id)
     .single();
 
@@ -32,53 +32,83 @@ export const GET: APIRoute = async ({ request, params, cookies }) => {
     path.resolve("./src/assets/JetBrainsMono-Bold.ttf"),
   );
   const JBregular = fs.readFileSync(
-    path.resolve("./src/assets/JetBrainsMono-Regular.ttf"),
+    path.resolve("./src/assets/JetBrainsMono-Medium.ttf"),
   );
+
+  const field = (key: string) => e("span", "text-teal-300", [key])
+
+  const row = (children: any[], classes?: string) => e("div", `flex ${classes}`, children)
+  const comment = (text: string) => e("span", "text-slate-500", text)
+  const hiString = (text: string) => e("span", "text-amber-300", text)
+  const hiVar = (text: string) => e("span", "text-orange-200", text)
+  const hiWarning = (text: string) => e("span", "text-red-400", text)
+  const hiConstructor = (text: string) => e("span", "text-teal-300", text)
+  const indent = (children: any[]) => e("span", "ml-[3rem]", children)
+  const val = (value: any) => {
+    if (value === "nil") {
+      return hiWarning("nil")
+    }
+
+    const type = typeof value;
+
+    if(type === "number") return e(
+      "span",
+      "text-purple-300 inline-block",
+      `${value}`,
+    )
+
+    return hiString(`"${value}"`)
+  }
+  const kv = (key: string, value: string | number) =>
+    e("div", "flex text-slate-500", [
+      field(key), " = ", val(value), ","
+    ])
 
   const html = e(
     "div",
-    "w-full h-full flex flex-col text-5xl items-center justify-center relative bg-slate-900 text-teal-400",
+    "w-full h-full flex flex-col text-4xl items-center justify-center relative bg-black text-teal-400 border rounded-2xl",
     [
+      {
+          type: "img",
+          props: {
+            src: import.meta.env.PUBLIC_AUTH_REDIRECT_HOST + "/logo.svg",
+            tw: "absolute -top-10 -left-20 opacity-25 -z-10",
+            width: 800,
+            height: 800,
+          }
+        },
+
       e(
         "div",
-        "absolute inset-12 border-4 flex flex-col border-teal-300 rounded-2xl bg-black p-8 mb-8",
-        [
-          e("p", "tracking-wide uppercase text-amber-400", [
-            "[",
-            e("span", "text-teal-400", "NEOVIM"),
-            e("span", "text-teal-800", "CONF"),
-            e("span", "text-teal-500", ".live"),
-            "]",
-          ]),
-          e("div", "pl-4 flex flex-col text-teal-200", [
-            e("p", "text-transparent", "-"),
-            e("p", "", [
-              "pass = ",
-              e(
-                "span",
-                "text-amber-400 ml-4",
-                ` "******#${id?.padStart(4, "0")}"`,
-              ),
-            ]),
-            e("p", "", [
-              "name = ",
-              e(
-                "span",
-                "text-amber-400 ml-4 inline-block",
-                ` "${user.username}"`,
-              ),
-            ]),
-            e("p", "", [
-              "job = ",
-              e("span", "text-amber-400 ml-4", ` "${user.job_description}"`),
-            ]),
-            e("p", "text-", [
-              e("span", "text-slate-600", "# aka: " + user.full_name),
-            ]),
-          ]),
-        ],
+        "flex absolute right-4 bottom-4 text-slate-700 text-xl border-b border-b-slate-700",
+        `verification: ${user.id.split("-").at(0)}-${(2023).toString(16)}-${Number(id).toString(16)}-${(Math.random() * 2000).toFixed(0).toString(16)}`
       ),
-    ],
+      {
+        type: "pre",
+        props: {
+          tw: "text-slate-500 flex flex-col bg-slate-800 p-8 rounded-lg border border-4 border-slate-600",
+          children: [
+            row(comment(`--- @see https://www.neovimconf.live`)),
+            row([
+              hiVar("require "),
+              hiString('"NEOVIMCONF.live" '),
+              hiVar(".permit "),
+              hiConstructor("{"),
+            ]),
+            row(indent(kv("date", "2023-12-08"))),
+            row(indent(kv("ticket_nr", id.padStart(4, "0")))),
+            user?.full_name && row(indent(comment(`-- aka ${user?.full_name}:`))),
+            row(indent(kv("username", "nil"))),
+            row(indent(kv("job_description", user?.job_description ?? "nil"))),
+            row(hiConstructor("}"))
+          ]
+        }
+      }, 
+    ], {
+    style: {
+      fontFamily: "JetBrains Mono Regular",
+    }
+  },
   );
 
   // @ts-ignore
