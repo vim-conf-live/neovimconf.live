ca013f3d78ce0c13ab0ec6cd65dbccfaebe24aab
Nils Riedemann <nils@bleepbloop.studio>
2023-11-05 14:38:32 +0100

    feat: make getCurrentUser and getTicket reject on error
    makes composing much nicer

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,15 +8,12 @@ export const supabase = createClient(
 
 export const getCurrentUser = async () => {
   const { data: { user } } = await supabase.auth.getUser()
-  return user
+  return user ? user : Promise.reject('No user found')
 }
 
 export const getTicket = async (user: User) => {
-  const ticket = await supabase
-    .from("profiles")
-    .select("ticket")
-    .eq("id", user.id)
-    .single();
-
-  return ticket.data?.ticket;
+  const ticket = await supabase.from("profiles").select("ticket").eq("id", user.id).single();
+  return ticket.data?.ticket 
+    ? ticket.data.ticket 
+    : Promise.reject('No ticket found');
 }
