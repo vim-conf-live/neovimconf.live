d07729f2537dcd6951cee621bce69994a511f1b3
Nils Riedemann <nils@bleepbloop.studio>
2023-11-11 00:57:04 +0100

    fix: disable sentry sourcemap for now

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -16,12 +16,10 @@ export default defineConfig({
   integrations: [
     sentry({
       dsn: "https://acc5a569e0cde4317688542f3df992a8@o192873.ingest.sentry.io/4506201758171136",
-      environment: env.NODE_ENV,
-      sourceMapsUploadOptions: {
-        project: "neovimconf",
-        telemetry: env.NODE_ENV === "production",
-        authToken: env.SENTRY_AUTH_TOKEN,
-      },
+      // sourceMapsUploadOptions: {
+      //   project: "neovimconf",
+      //   authToken: env.SENTRY_AUTH_TOKEN,
+      // },
     }),
     tailwind({ applyBaseStyles: false }), react()],
   adapter: netlify(),
