81ee2c210a945574511b195cce63b781869ec501
Nils Riedemann <nils@bleepbloop.studio>
2023-11-10 16:08:58 +0100

    feat: add sentry

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,7 +1,11 @@
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify/functions";
+import { defineConfig } from "astro/config";
+import sentry from "@sentry/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +13,14 @@ export default defineConfig({
   redirects: {
     "/talks": "/talks/day-1",
   },
-  integrations: [tailwind({ applyBaseStyles: false }), react()],
+  integrations: [
+    sentry({
+      dsn: "https://acc5a569e0cde4317688542f3df992a8@o192873.ingest.sentry.io/4506201758171136",
+      sourceMapsUploadOptions: {
+        project: "neovimconf",
+        authToken: env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+    tailwind({ applyBaseStyles: false }), react()],
   adapter: netlify(),
 });
