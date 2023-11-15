4f58e3d2100cd7acb931dd070495feec7dc9985f
Josh Medeski <josh.medeski@gmail.com>
2023-11-15 16:03:00 -0600

    feat: add sponsors

--- apps/web/src/content/config.ts
+++ apps/web/src/content/config.ts
@@ -2,23 +2,26 @@ import { z, defineCollection, reference } from "astro:content";
 
 const speakers = defineCollection({
   type: "content",
-  schema: ({image}) => (z.object({
-    name: z.string(),
-    talk: z.string().optional(),
-    links: z.array(z.string().url()).optional(),
-    type: z.union([z.literal("speaker"), z.literal("keynote"), z.literal("mc")]).optional(),
-    job: z.string(),
-    alias: z.string().optional(),
-    github: z.string().optional(),
-    info: z.string(),
-    website: z.string().optional(),
-    image: image().optional(),
-    terminal: z.string().optional(),
-    os: z.string().optional(),
-    shell: z.string().optional(),
-    colorscheme: z.string().optional(),
-    language: z.string(),
-  })),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      talk: z.string().optional(),
+      links: z.array(z.string().url()).optional(),
+      type: z
+        .union([z.literal("speaker"), z.literal("keynote"), z.literal("mc")])
+        .optional(),
+      job: z.string(),
+      alias: z.string().optional(),
+      github: z.string().optional(),
+      info: z.string(),
+      website: z.string().optional(),
+      image: image().optional(),
+      terminal: z.string().optional(),
+      os: z.string().optional(),
+      shell: z.string().optional(),
+      colorscheme: z.string().optional(),
+      language: z.string(),
+    }),
 });
 
 const schedules = defineCollection({
@@ -50,8 +53,22 @@ const talks = defineCollection({
   }),
 });
 
+const sponsors = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      slug: z.string(),
+      url: z.string().url(),
+      image: image(),
+      section: z.enum(["paladium", "community"]),
+      desc: z.string().optional(),
+    }),
+});
+
 export const collections = {
   speakers,
   talks,
   schedules,
+  sponsors,
 };
