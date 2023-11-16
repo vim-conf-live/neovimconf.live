16f1c27d8013d688d9c17cbbd3b482b36e8fa1c1
Nils Riedemann <nils@bleepbloop.studio>
2023-11-16 14:38:50 +0100

    feat: show twitch link when countdown is <= 0
    pointing to prime's twitch like in 2022

--- apps/web/src/components/CountDown.tsx
+++ apps/web/src/components/CountDown.tsx
@@ -2,10 +2,12 @@ import { useEffect, useRef, type TimeHTMLAttributes } from "react";
 
 const START_DATE = new Date('2023-12-08T10:00:00.000Z');
 
-const segments = (now: Date, target = START_DATE) => {
-  const millisecondsUntilTarget = START_DATE.getTime() - now.getTime();
-  const secondsUntilTarget = Math.floor(millisecondsUntilTarget / 1000);
+const msLeft = (now: Date, target = START_DATE) => {
+  return START_DATE.getTime() - now.getTime();
+}
 
+const createSegments = (millisecondsUntilTarget: number) => {
+  const secondsUntilTarget = Math.floor(millisecondsUntilTarget / 1000);
   return [
     Math.floor(secondsUntilTarget / 86400),
     Math.floor(secondsUntilTarget / 3600) % 24,
@@ -21,9 +23,14 @@ export const CountDown: React.FC<TimeHTMLAttributes<never>> = ({className}) => {
   useEffect(() => {
     const interval = setInterval(() => {
       if (!ref.current) return;
-      ref.current.innerText = segments(
-      new Date()).map((segment) => segment.toString().padStart(2, '0')).join(':');
-    }, 8)
+      const timeLeft = msLeft(new Date)
+      const segments = createSegments(timeLeft);
+      if (timeLeft <= 0) {
+        ref.current.innerHTML = `<a href="https://www.twitch.tv/theprimeagen" class="bg-purple-500 text-white py-1 px-2 rounded">LIVE ON TWITCH!</a>`;
+        return clearInterval(interval);
+      }
+      ref.current.innerText = segments.map((segment) => segment.toString().padStart(2, '0')).join(':');
+    }, 10)
 
     return () => clearInterval(interval)
   }, [ref.current])
