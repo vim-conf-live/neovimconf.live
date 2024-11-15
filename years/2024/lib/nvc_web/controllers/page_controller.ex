1db892a52920406a93e39e17e73dd1b05e406b6d
Nils Riedemann <nils@bleepbloop.studio>
2024-11-16 00:29:31 +0100

    feat: bring agenda back onto homepage

--- years/2024/lib/nvc_web/controllers/page_controller.ex
+++ years/2024/lib/nvc_web/controllers/page_controller.ex
@@ -3,15 +3,19 @@ defmodule NvcWeb.PageController do
   use NvcWeb, :controller
   alias Nvc.Speakers
   alias Nvc.Accounts
+  alias Nvc.Agenda
 
   def home(conn, _params) do
+    agenda_items = Agenda.list_agenda_items()
+      |> Agenda.calculate_start_times(~N[2024-11-19 15:00:00])
+
     render(conn, :home,
       changeset: Nvc.Accounts.change_user_registration(%User{}),
       justin: Speakers.get_speaker_by_handle!("justinmk"),
       keynote: Nvc.Speakers.list_speakers(:keynote),
       mcs: Nvc.Speakers.list_speakers(:mc),
       sponsors: Nvc.Sponsors.list_sponsors(),
-      agenda_items: Nvc.Agenda.list_agenda_items(),
+      agenda_items: agenda_items,
       speakers: Nvc.Speakers.list_speakers(:public_speakers)
     )
   end
